74d574b335994b330961a13f9cd4ef0f78f72ff6
DT7701 <51826812+DT7701@users.noreply.github.com>
2020-07-17 15:42:45 +0100

    dsadas
    das

--- index.js
+++ index.js
@@ -1,71 +1,121 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
-var del = document.getElementsByClassName("delBtn");
+let text1 = document.getElementById("sliderText1");
+
+let slider1 = document.getElementById("slider1");
+
+text1.textContent = slider1.value + "%";
+
+slider1.addEventListener("mousemove",function(){
+  text1.textContent = slider1.value + "%"
+})
+
+let text2 = document.getElementById("sliderText2");
+
+let slider2 = document.getElementById("slider2");
+
+text2.textContent = slider2.value + "%";
+
+slider2.addEventListener("mousemove",function(){
+  text2.textContent = slider2.value + "%"
+});
 
 
-
-function inputLength() {
-	return input.value.length;
+async function csvToDataObj(filename){
+  const response = await fetch(filename);
+  const data = await response.text();
+  const rows = data.split('\n').slice(1);
+  const dataObj = $.csv.toObjects(data);
+  
+  return dataObj;
 }
 
-function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	//creates a button and adds it inside the li
-	var btn = document.createElement("Button");
-	btn.appendChild(document.createTextNode("Delete"));
-	btn.classList.add('delBtn');
-	li.appendChild(btn);
-	ul.appendChild(li);
-	setButtons();
-	input.value = "";
+function formatDataObj(dataObj){
+  dataPoints=dataObj.map(obj=>{
+    let date = moment(obj.date,"DD-MM-YYYY");
+    timeStamp = date.valueOf()
+    let value = parseFloat(obj.Schroder)
+    let dataPoint={
+      x:timeStamp,
+      y:value
+    }
+   return dataPoint
+  })
+  return dataPoints
 }
 
-function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-	}
+function plotData(formattedDataObj){
+
+  var chart = new CanvasJS.Chart("chartContainer",
+  {
+    zoomEnabled: true,      
+      
+    title:{
+      text: "Chart With Date-Time Stamps Inputs"       
+    },
+
+    data: [
+    {        
+    type: "line",
+    xValueType: "dateTime",
+    dataPoints: formattedDataObj
+    }
+    ]
+  });
+  chart.render();
+ 
 }
 
-function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
-}
-
-//toggles done class to strikethrough
-function liClicked()
-{
-	console.log("li clicked");
-	var element = document.querySelector("li");
-  	element.classList.toggle("done");
-}
-
-//removes the li as parent element
-function deleteLi(e){
-	e.target.parentNode.remove();
-
-}
-
-function toggleStrike(e) {
-		if (e.target.tagName === 'LI')
-		{
-      	e.target.classList.toggle("done");
-  	}
-}
-
-//adds event listener to all buttons
-function setButtons(){
-	for (var i = 0; i <del.length; i++){
-		del[i].addEventListener('click', deleteLi);
-	}
-}
-
-button.addEventListener("click", addListAfterClick);
-
-input.addEventListener("keypress", addListAfterKeypress);
+let Data = new Promise(function(resolve,reject){
+  resolve(csvToDataObj("stockdata.csv"))})
+.then(dataObj=>formatDataObj(dataObj))
+.then(formattedDataObj=>plotData(formattedDataObj))
 
 
-//toggles underline 
-ul.addEventListener('click', toggleStrike);
+
+// window.onload = function () {
+//   var chart = new CanvasJS.Chart("chartContainer",
+//   {
+//     zoomEnabled: true,      
+    
+//     title:{
+//      text: "Chart With Date-Time Stamps Inputs"       
+//    },
+
+//    data: [
+//    {        
+//     type: "area",
+//     xValueType: "dateTime",
+//     dataPoints: [                  
+//             { x: 1088620200000, y :71},
+//             { x: 1104517800000, y : 55 },
+//             { x: 1112293800000, y:  50 },
+//             { x: 1136053800000, y : 65 },
+//             { x: 1157049000000, y : 95 },
+//             { x: 1162319400000, y : 68 },
+//             { x: 1180636200000, y : 28 },
+//             { x: 1193855400000, y : 34 },
+//             { x: 1209580200000, y : 14 },
+//             { x: 1230748200000, y : 34 },
+//             { x: 1241116200000, y : 44 },
+//             { x: 1262284200000, y : 84 },
+//             { x: 1272652200000, y : 4  },
+//             { x: 1291141800000, y : 44 },
+//             { x: 1304188200000, y : 11 }
+//     ]
+//   }
+//   ]
+// });
+
+//   chart.render();
+// }
+
+
+
+
+
+
+
+
+
+
+
+
