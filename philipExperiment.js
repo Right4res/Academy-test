6b3759c7b886965340858dcecae2892e896328f6
Philip Ridgill <68227913+Right4res@users.noreply.github.com>
2020-07-20 13:54:13 +0100

    improved pasuing functionality

--- philipExperiment.js
+++ philipExperiment.js
@@ -1,30 +1,47 @@
 const toggleClick = document.getElementById("pauseClick");
 const toggleEnter = document.getElementById("pauseEnter");
-const theYearIs = document.getElementById("theYearIs");
+// const theYearIs = document.getElementById("theYearIs");
 
 let pauseChart = false
+updateInterval = 1000;
+
 
 function renderChartAfterKeypress(event) {
-	if (event.keyCode === 13) {
+	if (event.keyCode === 13 | event.keyCode === 32) {
         pauseChart = !pauseChart;
+        if (pauseChart === false) {
+            idSetInterval = setInterval(function(){updateChart()}, updateInterval);
+            idSetIntervalShowYear = setInterval(function(){findYear()}, updateInterval);
+        } else {
+            clearInterval(idSetInterval);
+            clearInterval(idSetIntervalShowYear);
+        }
         console.log("event happened " + pauseChart);
 	}
 }
 
 function renderChartAfterToggle() {
     pauseChart = !pauseChart;
+    if (pauseChart === false) {
+        idSetInterval = setInterval(function(){updateChart()}, updateInterval);
+        idSetIntervalShowYear = setInterval(function(){findYear()}, updateInterval);
+    } else {
+        clearInterval(idSetInterval);
+        clearInterval(idSetIntervalShowYear);
+    }
     console.log(pauseChart)
 }
 
 function findYear() {
+    document.getElementById('yearShow').innerHTML = year;
     console.log(year);
-    document.getElementById('year').innerHTML = year-1
-}
+    sessionStorage.setItem("YearStored", "year");
+} 
 
 toggleClick.addEventListener("click", renderChartAfterToggle);
 
-toggleEnter.addEventListener("keypress", renderChartAfterKeypress);
+window.addEventListener("keypress", renderChartAfterKeypress);
 
-theYearIs.addEventListener("click", findYear);
+// theYearIs.addEventListener("click", findYear);
 
 
