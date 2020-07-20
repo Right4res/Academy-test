fc8ca1c41d662888caf9be9d3f232736f31919fc
Philip Ridgill <68227913+Right4res@users.noreply.github.com>
2020-07-20 14:50:21 +0100

    included sessionStorage

--- philipExperiment.js
+++ philipExperiment.js
@@ -35,8 +35,31 @@ function renderChartAfterToggle() {
 function findYear() {
     document.getElementById('yearShow').innerHTML = year;
     console.log(year);
-    sessionStorage.setItem("YearStored", "year");
+    sessionStorage.setItem("yearStored", year);
 } 
+
+// const data = await fetchData();
+
+async function fetchData() {
+    const date = [];
+    const cash = [];
+    const stocks = [];
+  
+    const response = await fetch("data/test.csv");
+    const data = await response.text();
+  
+    // console.log(data)
+  
+    const table = data.split("\n").slice(1); // line break from row 2 to end
+    table.forEach(row => {
+        const column = row.split(',');
+        date.push(column[0]);
+        cash.push(parseFloat(column[1]));
+        stocks.push(parseFloat(column[2]));
+        console.log(date, cash, stocks);
+    });
+    return { date, cash, stocks }
+}
 
 toggleClick.addEventListener("click", renderChartAfterToggle);
 
